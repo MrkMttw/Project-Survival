9bd0a55be24a8e7bc1851086432cf6882d896d35
MrkMttw <alingcastremarkmatthew@gmail.com>
2026-08-18 07:20:27 +0800

    Added Sprint and Expanded Map size
    I added sprint mechanics and increased the map size.

---
+++ Assets/Scripts/MapTransition.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class MapTransition : MonoBehaviour
+{
+    [SerializeField] PolygonCollider2D mapBoundary;
+    [SerializeField] CinemachineConfiner2D confiner;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            confiner.BoundingShape2D = mapBoundary;
+            confiner.InvalidateBoundingShapeCache();
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,23 +1,66 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]private float moveSpeed = 5f;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float sprintMultiplier = 2f;
+    [SerializeField] private float sprintDuration = 3f;
+    [SerializeField] private float sprintCooldown = 5f;
+
+    private float sprintTimer;
+    private float cooldownTimer;
+
     private Rigidbody2D rb;
     private Vector2 moveInput;
     private Animator animator;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+
+        // Start with a full sprint
+        sprintTimer = sprintDuration;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = moveInput * moveSpeed;
+        // Cooldown timer
+        if (cooldownTimer > 0)
+        {
+            cooldownTimer -= Time.deltaTime;
+
+            // Cooldown finished
+            if (cooldownTimer <= 0)
+            {
+                cooldownTimer = 0;
+                sprintTimer = sprintDuration;
+            }
+        }
+
+        float currentSpeed = moveSpeed;
+
+        // Sprint
+        if (Input.GetKey(KeyCode.LeftShift) &&
+            sprintTimer > 0 &&
+            cooldownTimer <= 0)
+        {
+            currentSpeed *= sprintMultiplier;
+
+            sprintTimer -= Time.deltaTime;
+
+            // Sprint finished
+            if (sprintTimer <= 0)
+            {
+                sprintTimer = 0;
+                cooldownTimer = sprintCooldown;
+            }
+        }
+
+        rb.linearVelocity = moveInput * currentSpeed;
     }
+
     public void OnMove(InputAction.CallbackContext context)
     {
         animator.SetBool("isWalking", true);
@@ -30,6 +73,7 @@ public class PlayerMovement : MonoBehaviour
         }
 
         moveInput = context.ReadValue<Vector2>();
+
         animator.SetFloat("InputX", moveInput.x);
         animator.SetFloat("InputY", moveInput.y);
     }
